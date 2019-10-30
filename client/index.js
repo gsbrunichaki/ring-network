a1a782e90bbe91fd8218569307132e254bde2539
marceloBernardy <marcelo.azevedo@acertsis.com.br>
2019-10-29 22:10:53 -0300

    adding parameters on start file

--- client/index.js
+++ client/index.js
@@ -1,8 +1,21 @@
-var PORT = 5000;
-var HOST = "127.0.0.1";
-
-var dgram = require("dgram");
+const dgram = require("dgram");
 var message = new Buffer("My KungFu is Good!");
+
+const configFile = {
+  ip_destino_token: process.env.ip,
+  apelido_da_maquina_atual: process.env.apelido,
+  tempo_token: process.env.tempo,
+  token: process.env.token
+};
+
+console.log(configFile);
+
+configFile.ip_destino_token = process.env.ip;
+configFile.apelido_da_maquina_atual = process.env.apelido;
+configFile.tempo_token = process.env.tempo;
+configFile.token = process.env.token;
+
+console.log("config", configFile);
 
 var client = dgram.createSocket("udp4");
 client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
--- configFile.js
+++
@@ -1,6 +0,0 @@
-export default {
-  ip_destino_token: "192.168.1.12:6000",
-  apelido_da_maquina_atual: "Bob",
-  tempo_token: "1",
-  token: true
-};
