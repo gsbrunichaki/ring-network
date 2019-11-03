0ac710b332702840d50de459cb9268f6b4de5d83
marceloBernardy <marcelo.azevedo@acertsis.com.br>
2019-11-03 12:43:01 -0300

    fix send message

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const dgram = require("dgram");
 const server = dgram.createSocket("udp4");
 
-const HOST = "127.0.0.1";
-const PORT = "5000";
+const PORT = "41234";
 
 server.on("error", err => {
   console.log(`server error:\n${err.stack}`);
   server.close();
 });
 
-//send package
+//receive package
 server.on("message", (msg, rinfo) => {
   console.log(rinfo);
   console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
 });
 
-//receive package
+//listening package
 server.on("listening", () => {
   ///logic aqui dentro
   const address = server.address();
   console.log(`server listening ${address.address}:${address.port}`);
 });
 
-server.bind(PORT, HOST);
+server.bind(PORT);
